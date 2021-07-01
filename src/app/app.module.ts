3af081efcb5be6b9a1bf03eb478694508b727f44
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-07-01 23:02:24 +0900

    ⚡️ :: [App] exception filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpErrorFilter } from 'src/exception-filter';
 import { MealModule } from '../meal/meal.module';
 import { SchoolModule } from '../school/school.module';
 
 @Module({
   imports: [SchoolModule, MealModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter
+    }
+  ]
 })
 export class AppModule {}
