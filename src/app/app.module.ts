729c837bd678191710548efa5c7cf04a37405465
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-07-01 23:08:05 +0900

    :bug: :: [App] file path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
-import { HttpErrorFilter } from 'src/exception-filter';
+import { HttpErrorFilter } from '../exception-filter';
 import { MealModule } from '../meal/meal.module';
 import { SchoolModule } from '../school/school.module';
 
